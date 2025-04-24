ea22ae5de7a60cf5b67a9852b5a039dc4febbca2
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-04-24 01:12:33 -0500

    Update banking.py

--- banking.py
+++ banking.py
@@ -37,36 +37,129 @@ def main():
     #New interpreter and call initialize
     interpreter = Interpreter.Interpreter()
     initialize(interpreter)
-    
-    #Print available accounts
-    print("Available accounts:")
-    for acc in interpreter.bank:
-        print(f" - {acc}")
 
-    #Print Instructions
-    print("To leave, type 'Exit'\n")
-    print("To run specification testing, type 'Test'\n")
-    print("Command syntax:\nCreate firstName lastName\nCheck Balance accountNumber\nWithraw decimalNumber from accountNumber\nDeposit decimalNumber to accountNumber")
-    print("\n\n")
-
-    #Input loop
+    #Loop for main logic
     stay = True
     while stay:
-        commands = []
-        userInput = input("Command: ")
+        #Print options
+        print("\nSelect from the following options.")
+        print("1. Login to an account.")
+        print("2. Create new account")
+        print("3. Use Console (Actually using our language)")
+        print("4. Unit Test")
+        print("5. Exit.")
+
+        userInput = input("Selection: ").strip()
+
+        #Switch statement for user input
+        match userInput:
+            case "1": #Log into accout
+                #Print available accounts
+                print("Available accounts:")
+                for acc in interpreter.bank:
+                    print(f" - {acc}")
+                    
+                accountID = input("Enter an account to login to  ").strip()
+                
+                #Error handling if account does not exist
+                if accountID not in interpreter.bank:
+                    print("Account not found.")
+                else: #If account exists
+                    #Input loop for logged in logic
+                    stayLogged = True
+                    while stayLogged:
+                        #Print options
+                        print("\nSelect from the following options.")
+                        print("1. Make a deposit.")
+                        print("2. Make a withdrawal.")
+                        print("3. Check the balance.")
+                        print("4. Log out.")
+                        
+                        userInput = input("Selection: ").strip()
+
+                        #Switch for user input
+                        match userInput:
+                            case "1": #Make deposit based on input
+                                #Get input
+                                userInput = input("Enter the amount to deposit. Example: '10', '15.55': ")
+
+                                #Ensures input is digit and compiles input
+                                if userInput.isdigit():
+                                    inputString = f"Deposit {userInput} to {accountID}"
+                                    inputArray = [inputString]
+                                    runCommands(inputArray, interpreter)
+                                else:
+                                    print("Invalid Entry")
+                            case "2": #Make withdrawl based on input
+                                #Get input
+                                userInput = input("Enter the amount to withdraw. Example: '10', '15.55': ")
+
+                                #Ensures input is digit and compiles input
+                                if userInput.isdigit():
+                                    inputString = f"Withdraw {userInput} from {accountID}"
+                                    inputArray = [inputString]
+                                    runCommands(inputArray, interpreter)
+                                else:
+                                    print("Invalid Entry")
+                            case "3": #Check balance of account
+                                #Compiles command
+                                inputString = f"Check Balance {accountID}"
+                                inputArray = [inputString]
+                                runCommands(inputArray, interpreter)
+                            case "4": #Log out
+                                stayLogged = False
+                            case default: #Incorrect input handling
+                                print("Invalid Option")
+            case "2": #Create new account
+                #Get input
+                userInput = input("Enter the first and last name only. Example: 'Ty Steinbach': ")
+
+                #Compile commands
+                try:
+                    inputString = f"Create {userInput}"
+                    inputArray = [inputString]
+                    runCommands(inputArray, interpreter)
+                except:
+                    print("Invalid Entry")
+            case "3": #Use console
+                #Print instructions
+                print("To leave, type 'Exit'\n")
+                print("Command syntax:\nCreate firstName lastName\nCheck Balance accountNumber\nWithraw decimalNumber from accountNumber\nDeposit decimalNumber to accountNumber")
+                print("\n\n")
+
+                #Input loop for console logic
+                stayConsole = True
+                while stayConsole:
+                    commands = []
+                    userInput = input("Command: ")
+                        
+                    if userInput == "Exit": #Exit
+                        stayConsole = False
+                    else: #Compile
+                        commands.append(userInput)
+                        try:
+                            runCommands(commands, interpreter)
+                        except:
+                            print("Incorrect Syntax")
+            case "4": #Run unit tests
+                specification_tests()
+            case "5": #Exit
+                stay = False
+            case default: #For any other input
+                print("Invalid Option")
         
-        if userInput == "Exit": #Exit
-            stay = False
-        elif userInput == "Test": #Run Tests
-            specification_tests()
-        else: #Compile
-            commands.append(userInput)
-            try:
-                tokens = Lexer.Lexer.getTokens(commands)
-                AST = Parser.Parser.parse(tokens)
-                interpreter.interpret(AST)
-            except:
-                print("Incorrect Syntax")
+                        
+            
+                    
+
+###################################
+#####       runCommands       #####
+###################################
+#Runs tests
+def runCommands(commands, interpreter):
+    tokens = Lexer.Lexer.getTokens(commands)
+    AST = Parser.Parser.parse(tokens)
+    interpreter.interpret(AST)
 
 ###################################
 #####    specification_tests  #####
