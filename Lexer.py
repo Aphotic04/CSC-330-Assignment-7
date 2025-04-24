c6307fdbc51be7cbe5215e870f05d93aa45c35d8
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-04-24 00:05:02 -0500

    Update Lexer.py

--- Lexer.py
+++ Lexer.py
@@ -8,72 +8,67 @@
 import re
 import Token
 
+###################################
+#####          Lexer          #####
+###################################
 class Lexer:
     def getTokens(linesCode):
         tokens = []
         for line in linesCode:
+            #If string matches a deposit or withdraw
             if re.search("^((Withdraw|Deposit) [0-9]+[.][0-9]+ (to|from) [a-zA-Z]{2}[0-9]{6})$", line):
+                #Get amount/account info
                 amount = float(re.search("[0-9]+[.][0-9]+", line).group())
                 account = re.search("([a-zA-Z]{2}[0-9]{6})$", line).group()
-                
+                #Create tokens from amount/account
                 amountToken = Token.Token(Token.TokenType.NUMBER, amount)
-                #amountToken.setType(Token.TokenType.Number)
-                #amountToken.setValue(amount)
-
                 accountToken = Token.Token(Token.TokenType.ACCOUNTID, account)
-                #accountToken.setType(Token.TokenType.AccountNumber)
-                #accountToken.setValue(amount)
-                
-                if (re.search("^Withdraw", line) and re.search("from", line)) or (re.search("^Deposit", line) and re.search("to", line)):
-                    action = re.search("Withdraw|Deposit", line).group()
 
-                    #actionToken = Token.Token
+                #Ensuring verb and preposition match
+                if (re.search("^Withdraw", line) and re.search("from", line)) or (re.search("^Deposit", line) and re.search("to", line)):
+                    #Create appropriate tokens
+                    action = re.search("Withdraw|Deposit", line).group()
 
                     if (action == "Withdraw"):
                         actionToken = Token.Token(Token.TokenType.WITHDRAW, Token.TokenType.WITHDRAW)
-                        #actionToken.setType(Token.TokenType.Withdraw)
-                        #actionToken.setValue(Token.TokenType.Withdraw)
+
                     else:
                         actionToken = Token.Token(Token.TokenType.DEPOSIT, Token.TokenType.DEPOSIT)
-                        #actionToken.setType(Token.TokenType.Deposit)
-                        #actionToken.setValue(Token.TokenType.Deposit)
+
+                    #And append
                     tokens.append(actionToken)
                     tokens.append(amountToken)
                     tokens.append(accountToken)
-                    #print(tokens)
                     
-                    print("Withdrawal or deposit")
                 else:
                     print("Verb and preposition don't match")
             
-            #Check Balance
+            #Mathces Check Balance
             elif re.search("^Check Balance [a-zA-Z]{2}[0-9]{6}$", line):
+                #Gets information and creates tokens
                 account = re.search("([a-zA-Z]{2}[0-9]{6})$", line).group()
 
                 actionToken = Token.Token(Token.TokenType.CHECK, Token.TokenType.CHECK)
                 accountToken = Token.Token(Token.TokenType.ACCOUNTID, account)
 
+                #Append tokens
                 tokens.append(actionToken)
                 tokens.append(accountToken)
-                #print(tokens)
-            #Create name
+            #Mathces Create name
             elif re.search("Create [a-zA-Z]+ [a-zA-Z]+$", line):
                 #Extract names to assist in accountID creation
                 names = re.findall("[a-zA-Z]+", line)
                 firstName, lastName = names[1], names[2]
                 
+                #Create appropriate tokens
                 actionToken = Token.Token(Token.TokenType.CREATE, Token.TokenType.CREATE)
                 firstNameToken = Token.Token(Token.TokenType.FIRSTNAME, firstName)
                 lastNameToken = Token.Token(Token.TokenType.LASTNAME, lastName)
-                
+
+                #Append tokens
                 tokens.append(actionToken)
                 tokens.append(firstNameToken)
                 tokens.append(lastNameToken)
-                #print(tokens)
             else:
-                print("What the heck happened?!")
+                print("Lexer Error: Incorrect Syntax")
         return tokens
-'''Lexer.getTokens({"Deposit 14.14 to TS223344"})
-Lexer.getTokens({"Withdraw 50.14 from TS223344"})
-Lexer.getTokens({"Check Balance TS223344"})
-Lexer.getTokens({"Create John Doe"})'''
