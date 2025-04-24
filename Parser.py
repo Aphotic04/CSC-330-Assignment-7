c47c7ebf9e448076c03afe9d83c536be2df69b27
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-04-24 00:13:52 -0500

    Update Parser.py

--- Parser.py
+++ Parser.py
@@ -9,32 +9,23 @@ import ASTNode
 import Lexer
 from Token import TokenType
 
+###################################
+#####         Parser          #####
+###################################
 class Parser:
-    #def __init__(self, tokens):
-    #    self.tokens = tokens
-
+    #Parses tokens
     def parse(tokens):
+        #Create new root node
         root = ASTNode.ASTNode()
         for x in range(len(tokens)):
-            #print(tokens[x].TokenType)
+            #For the appropriate token type, create appropriate tokens and their respective dependant values
             match tokens[x].TokenType:
                 case TokenType.CREATE:
                     root.expressions.append(ASTNode.Create(tokens[x + 1].value, tokens[x + 2].value))
-                    #ASTNode.ASTNode(self.tokens[1], ASTNode.ASTNode(self.tokens[0]), ASTNode.ASTNode(self.tokens[2]))
                 case TokenType.CHECK:
                     root.expressions.append(ASTNode.Check(tokens[x + 1].value))
-                    #return ASTNode.ASTNode(self.tokens[1], ASTNode.ASTNode(self.tokens[0]))
                 case TokenType.DEPOSIT:
                     root.expressions.append(ASTNode.Deposit(tokens[x + 1].value, tokens[x + 2].value))
-                    #return ASTNode.ASTNode(self.tokens[1], ASTNode.ASTNode(self.tokens[0]), ASTNode.ASTNode(self.tokens[2]))
                 case TokenType.WITHDRAW:
                     root.expressions.append(ASTNode.Withdraw(tokens[x + 1].value, tokens[x + 2].value))
-                    #return ASTNode.ASTNode(self.tokens[1], ASTNode.ASTNode(self.tokens[0]), ASTNode.ASTNode(self.tokens[2]))
-                
         return root
-            
-tokens = Lexer.Lexer.getTokens({"Deposit 14.14 to TS223344", "Withdraw 50.14 from TS223344", "Check Balance TS223344", "Create John Doe"})
-
-
-#root = Parser.parse(tokens)
-#print(root)
