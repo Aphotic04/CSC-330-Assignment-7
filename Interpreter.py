a6fc9fddb3dff9d5c204ddb74e1cf7ddf8aed31c
Ty Steinbach <152814492+Aphotic04@users.noreply.github.com>
2025-04-24 00:12:00 -0500

    Update Interpreter.py

--- Interpreter.py
+++ Interpreter.py
@@ -9,44 +9,42 @@ import Lexer
 import Parser
 import BankAccount
 
+###################################
+#####       Interpreter       #####
+###################################
 class Interpreter:
+    #Sets up bank dictionary
     def __init__(self):
         self.bank = {}
+    #Interpret an ast
     def interpret(self, ast):
         expressions = ast.expressions
+        #For each expression
         for x in range(len(expressions)):
             currExpr = expressions[x]
+            #If Create, create a new account
             if (isinstance(currExpr, ASTNode.Create)):
                 account = BankAccount.BankAccount(currExpr.firstName, currExpr.lastName)
                 returnNum = account.getAccountNumber()
                 self.bank[returnNum] = account
+            #If Check, check account
             if (isinstance(currExpr, ASTNode.Check)):
                 try:
                     num = currExpr.accountNumber
                     self.bank[num].checkBalance(num)
                 except:
                     print("Check Failed: Couldn't Find Account")
+            #If Deposit, deposit into account
             if (isinstance(currExpr, ASTNode.Deposit)):
                 try:
                     num = currExpr.accountNumber
                     self.bank[num].deposit(num, currExpr.amount)
                 except:
                     print("Deposit Failed: Couldn't Find Account")
+            #If Withdraw, withdraw from account
             if (isinstance(currExpr, ASTNode.Withdraw)):
                 try:
                     num = currExpr.accountNumber
                     self.bank[num].withdraw(num, currExpr.amount)
                 except:
                     print("Withdraw Failed: Couldn't Find Account")
-
-'''tokens = Lexer.Lexer.getTokens(["Create John Doe"])
-
-root = Parser.Parser.parse(tokens)
-interpreter = Interpreter()
-interpreter.interpret(root)
-account = list(interpreter.bank.keys())[0]
-
-tokens = Lexer.Lexer.getTokens([f"Deposit 14.14 to {account}", f"Withdraw 3.14 from {account}", f"Check Balance {account}"])
-root = Parser.Parser.parse(tokens)
-interpreter.interpret(root)'''
-
